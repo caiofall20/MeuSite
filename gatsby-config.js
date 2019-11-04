b489db886ea888d76a8fb3c97c9afc63f453a6e9
caio <caiomateusgames@gmail.com>
2019-11-04 14:41:54 -0300

    Finalizando o componente de pesquisa.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
-require("dotenv").config({
-  path: `.env.${process.env.NODE_ENV}`,
-})
+require("dotenv").config()
+
+const queries = require("./src/utils/algolia_queries")
 module.exports = {
   siteMetadata: {
     title: `My blog`,
@@ -58,6 +58,17 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-algolia-search`,
+      options: {
+        appId: process.env.ALGOLIA_APP_ID,
+        apiKey: process.env.ALGOLIA_ADMIN_KEY,
+        indexName: process.env.ALGOLIA_INDEX_NAME,
+        queries,
+        chunkSize: 10000, // default: 1000
+        enablePartialUpdates: true,
+      },
+    },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
